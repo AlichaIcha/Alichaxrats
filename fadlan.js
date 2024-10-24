c3fe38d3072da039f78ea704675110f00494a1a4
AlichaXD <dheriap248@gmail.com>
2024-10-25 00:00:21 +0700

    Add files via upload

--- fadlan.js
+++ fadlan.js
@@ -5,6 +5,7 @@ const fs = require('fs');
 const path = require('path');
 const readline = require('readline')
 const usePairingCode = process.argv.includes("--code") || process.argv.includes("--pairing")
+const PhoneNumber = require('awesome-phonenumber')
 const serviceAccount = require('./mmkcok.json');
 
 // Inisialisasi Firebase Admin
@@ -90,96 +91,46 @@ function deleteInvalidData() {
 }
 
 // Inisialisasi WhatsApp Client
-const connectionOptions = {
-	printQRInTerminal: !isPairing,
-	syncFullHistory: true,
-	markOnlineOnConnect: true,
-	connectTimeoutMs: 60000, 
-	defaultQueryTimeoutMs: 0,
-	keepAliveIntervalMs: 10000,
-	generateHighQualityLinkPreview: true, 
-	patchMessageBeforeSending: (message) => {
-		const requiresPatch = !!(
-			message.buttonsMessage 
-			|| message.templateMessage
-			|| message.listMessage
-		);
-		if (requiresPatch) {
-			message = {
-				viewOnceMessage: {
-					message: {
-						messageContextInfo: {
-							deviceListMetadataVersion: 2,
-							deviceListMetadata: {},
-						},
-						...message,
-					},
-				},
-			};
-		}
-
-		return message;
-	},
-	version: (await (await fetch('https://raw.githubusercontent.com/WhiskeySockets/Baileys/master/src/Defaults/baileys-version.json')).json()).version,
-	browser: ['Ubuntu', 'Chrome', '20.0.04'],
-	logger: pino({ level: 'fatal' }),
-	auth: { 
-		creds: state.creds, 
-		keys: makeCacheableSignalKeyStore(state.keys, pino().child({ 
-			level: 'silent', 
-			stream: 'store' 
-		})), 
-	},
-}
-
-const getMessage = async key => {
-	const messageData = await store.loadMessage(key.remoteJid, key.id);
-	return messageData?.message || undefined;
-}
-
-global.conn = simple.makeWASocket(connectionOptions)
-client.isInit = false
-
-if (!opts['test']) {
-	if (global.db) setInterval(async () => {
-		if (global.db.data) await global.db.write()
-		if (!opts['tmp'] && (global.support || {}).find) (tmp = [os.tmpdir(), 'tmp'], tmp.forEach(filename => cp.spawn('find', [filename, '-amin', '3', '-type', 'f', '-delete'])))
-	}, 30 * 1000)
-}
-
-async function connectionUpdate(update) {
-	const { connection, lastDisconnect } = update
-	global.timestamp.connect = new Date
-	if (lastDisconnect && lastDisconnect.error && lastDisconnect.error.output && lastDisconnect.error.output.statusCode !== DisconnectReason.loggedOut && consolws.readyState !== WebSocket.CONNECTING) {
-		console.log(global.reloadHandler(true))
-	}
-	if (global.db.data == null) await loadDatabase()
-	// console.log(JSON.stringify(update, null, 4))
-}
-	 if((usePairingCode || useMobile) && fs.existsSync('./konek/creds.json') && !client.authState.creds.registered) {
-		client.log(chalk.yellow('-- WARNING: creds.json is broken, please delete it first --'))
-		process.exit(0)
-	}
-	 if(isPairing && !client.authState.creds.registered) {
-			if(useMobile) throw new Error('Tidak dapat menggunakan Pairing Baileys API!')
-			const { registration } = { registration: {} }
-			let phoneNumber = ''
-			do {
-					phoneNumber = await question(chalk.yellowBright('Masukkan nomor yang valid, dengan Region: 62xxx:\nContoh: 6285888777444\n'))
-			} while (!Object.keys(PHONENUMBER_MCC).some(v => phoneNumber.startsWith(v)))
-			rl.close()
-			phoneNumber = phoneNumber.replace(/\D/g,'')
-			client.log(chalk.bgWhite(chalk.blue('Tunggu Sebentar...')))
-			setTimeout(async () => {
-					let code = await client.requestPairingCode(phoneNumber)
-					code = code?.match(/.{1,4}/g)?.join('-') || code
-					client.log(chalk.black(chalk.bgGreen(`Your Pairing Code : `)), chalk.black(chalk.white(code)))
-			}, 3000)
-		}
-
-client.on('ready', () => {
-    console.log('Berhasil Aktifkan Bot Ngab\n\nSc By 081400346604\n@FadlanDev');
+async function connectToWhatsApp() {
+const { state, saveCreds } = await useMultiFileAuthState(`session/${global.sessionName}`)
+const Senchini = makeWASocket({
+logger: pino({ level: "silent" }),
+printQRInTerminal: !usePairingCode,
+auth: state,
+browser: ["Ubuntu", "Chrome", "20.0.04"],
 });
+if(usePairingCode && !Senchini.authState.creds.registered) {
+        await clearConsole();
+		client.log(`The process of connecting to ${bottz}`)
+		setTimeout(async () => {
+          code = await Senchini.requestPairingCode(bottz)
+          code = code?.match(/.{1,4}/g)?.join("-") || code
+          client.log(`Pairing code: ${code.toUpperCase()}`);
+        }, 3000)
+
+	}
+async function clearConsole() {
+    const isWindows = process.platform === 'win32';
+    const isLinuxOrMac = process.platform === 'linux' || process.platform === 'darwin';
+
+    return new Promise((resolve, reject) => {
+        const command = isWindows ? 'cls' : (isLinuxOrMac ? 'clear' : '');
+        if (command) {
+            require('child_process').exec(command, (error, stdout, stderr) => {
+                if (error) {
+                    console.error(error);
+                    reject(error);
+                } else {
+                    console.log(stdout);
+                    resolve();
+                }
+            });
+        } else {
+            console.log('Berhasil Aktifkan Bot Ngab\n\nSc By 081400346604\n@FadlanDev');
+            resolve();
+        }
+    });
+}
 
 client.on('message', async (message) => {
     const chatId = message.from;
